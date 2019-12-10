443e57d07768ccf667cd4ae4246cdf539f173b6e
huayunh <hhuayun0828@gmail.com>
2019-12-10 11:42:56 -0500

    import Chinna's proj files; remove sharp from package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DataService } from './data.service';
+import * as Colors from '@pxblue/colors';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  list = [];
+  toggleNotification = false;
+  Colors: Object = Colors;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
+
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit() {
+    this.list = this.dataService.data;
   }
 
   initializeApp() {
@@ -23,5 +35,11 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  openNotification() {
+    this.toggleNotification = true;
+  }
+  closeNotification() {
+    this.toggleNotification = false;
   }
 }
